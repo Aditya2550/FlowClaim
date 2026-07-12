4c84878a8e0fda9685c61a697255356a58e1cd98
Aditya Valsangkar <adidev2550@gmail.com>
2026-07-13 00:49:20 +0530

    Update: removed the approval and workflow routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -3,8 +3,6 @@ import authRoutes from "../modules/auth/auth.routes.js";
 import userRoutes from "../modules/users/users.routes.js";
 import companyRoutes from "../modules/companies/companies.routes.js";
 import expenseRoutes from "../modules/expenses/expenses.routes.js";
-import approvalRoutes from "../modules/approvals/approvals.routes.js";
-import workflowRoutes from "../modules/workflows/workflows.routes.js";
 import analyticsRoutes from "../modules/analytics/analytics.routes.js";
 import notificationRoutes from "../modules/notifications/notifications.routes.js";
 
@@ -15,8 +13,6 @@ router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/companies", companyRoutes);
 router.use("/expenses", expenseRoutes);
-router.use("/approvals", approvalRoutes);
-router.use("/workflows", workflowRoutes);
 router.use("/analytics", analyticsRoutes);
 router.use("/notifications", notificationRoutes);
 
